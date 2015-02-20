106f67ce35bd91d0f8ce9df50cac00dfe178ad06
bhowland <bhowland@madisoncollege.edu>
2015-02-20 19:08:29 +0000

    finished with home and about us

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,11 +4,13 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get index" do
     get :index
     assert_response :success
+      assert_select "title", "Home | Guild App"
   end
 
   test "should get about_us" do
     get :about_us
     assert_response :success
+    assert_select "title", "About us | Guild App"
   end
 
 end
