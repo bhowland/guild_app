14188073a313a002532b0ba51bd454594ed3a3bc
bhowland <bhowland@madisoncollege.edu>
2015-04-22 05:59:19 +0000

    working log in

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ActiveRecord::Base
     has_many :reply_message
     attr_accessor :remember_token
     #for testing
-  before_save { self.email = email.downcase , self.name = name.downcase }
+  before_save { self.email = email.downcase}
   validates :name, presence: true, length: { maximum: 50 }, uniqueness: { case_sensitive: false }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 },
