67dc2cfb8677f8670e3bfb036c3b38f3958f6b1b
bhowland <bhowland@madisoncollege.edu>
2015-04-23 03:47:25 +0000

    check-in-2

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150422163229) do
+ActiveRecord::Schema.define(version: 20150423031422) do
 
   create_table "messages", force: :cascade do |t|
     t.string   "originalMessage"
@@ -38,6 +38,8 @@ ActiveRecord::Schema.define(version: 20150422163229) do
     t.string   "activation_digest"
     t.boolean  "activated",         default: false
     t.datetime "activated_at"
+    t.string   "reset_digest"
+    t.datetime "reset_sent_at"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
